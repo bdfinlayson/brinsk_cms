f24ce2b30f4186ba055b7fbc8f931145d060d36d
Bryan Finlayson <bdfinlayson@gmail.com>
2015-06-16 12:49:15 -0500

    Add tests for new user registration path

--- spec/views/authentications/authentication_pages_spec.rb
+++ spec/views/authentications/authentication_pages_spec.rb
@@ -9,13 +9,18 @@ describe 'Authentication' do
 
     it { should have_content('Sign up') }
     it { should have_title('Sign up') }
+    it { should have_content('First Name') }
+    it { should have_content('Last Name') }
+    it { should have_content('Email') }
+    it { should have_content('Password') }
+    it { should have_content('Confirm Password') }
   end
 
-  describe 'sign up' do
+  describe 'creating a new account' do
     before { visit new_user_registration_path }
 
     describe 'with invalid information' do
-      before { click_button 'Sign up' }
+      before { click_button 'Create my account' }
 
       it { should have_title('Sign up') }
       it { should have_selector('div#error_explanation') }
