f72dc7a54fa8a9d8726a73344d699072680cb174
Bryan Finlayson <bdfinlayson@gmail.com>
2015-06-17 19:31:28 -0500

    Add user_id to Projects table
    `rails generate migration AddUserIdToProjects user_id:integer
    `rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150617214615) do
+ActiveRecord::Schema.define(version: 20150618002721) do
 
   create_table "contacts", force: :cascade do |t|
     t.integer  "user_id"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20150617214615) do
     t.boolean  "completed"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
