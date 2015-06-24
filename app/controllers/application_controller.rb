5f972d1c7b61373414a527b3ff697eec30910068
Bryan Finlayson <bdfinlayson@gmail.com>
2015-06-23 19:24:58 -0500

    Add styling for authentication pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_filter :configure_devise_params, if: :devise_controller?
+
   def configure_devise_params
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:first_name, :last_name, :email, :password, :password_confirmation)
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,8 +4,8 @@ class Contact < ActiveRecord::Base
   validates :last_name, presence: true, length: { maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
-  validates_uniqueness_of :email
   before_save { email.downcase! }
+  validates_uniqueness_of :email, scope: :user_id
   before_save { self.email = email.downcase }
   has_many :notes, dependent: :destroy
   has_many :projects, dependent: :destroy
