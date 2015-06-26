d6d2b82583407053d670a7c91397896ac711db2b
Bryan Finlayson <bdfinlayson@gmail.com>
2015-06-25 20:18:03 -0500

    Add gravatar support

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,2 +1,8 @@
 module ContactsHelper
+
+  def gravatar_for(contact)
+    gravatar_id = Digest::MD5::hexdigest(contact.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: contact.first_name, class: 'gravatar')
+  end
 end
