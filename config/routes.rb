150912900cf3ea0cdd9f9874c743914d881583bc
Bryan Finlayson <bdfinlayson@gmail.com>
2016-11-06 14:38:25 -0600

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,48 +2,17 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :contacts do
-    member do
-      post :email_contact
-    end
-    resources :projects do
-      resources :notes, except: [:show]
-      resources :stages do
-        resources :tasks do
-          member do
-            patch :complete
-          end
-        end
-      end
-      member do
-        patch :complete
-      end
-      resources :tasks, except: [:show] do
-        member do
-          patch :complete
-        end
-      end
-    end
-    resources :tasks, except: [:show] do
-      member do
-        patch :complete
-      end
-    end
+    resources :tasks, except: [:show]
     resources :notes
   end
 
   resources :projects do
     member do
       patch :complete
-      post :email_contact
     end
   end
-  resources :stages, except: [:show]
   resources :notes, except: [:show]
-  resources :tasks, except: [:show] do
-    member do
-      patch :complete
-    end
-  end
+  resources :tasks, except: [:show]
 
   root "contacts#index"
 
