bca121ce1f005ed3d96cfc84aa3102e583670e13
Bryan Finlayson <bdfinlayson@gmail.com>
2016-11-12 07:57:32 -0600

    Add data table search to contacts index, show

--- app/assets/javascripts/tables.coffee
+++ app/assets/javascripts/tables.coffee
@@ -1,2 +1,2 @@
 $ ->
-  $('#tags').dataTable()
+  $('.dataTable').dataTable();
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,15 +3,12 @@ class ContactsController < ApplicationController
   def index
     @user = current_user
     @contacts = current_user.contacts.all
-    @search = Contact.search(params[:search])
-    @contacts = @search.select { |contact| contact[:user_id] == current_user.id } unless @search.empty?
+    @contacts = @search.select { |contact| contact[:user_id] == current_user.id }
   end
 
   def show
     @contact = Contact.find(params[:id])
     @notes = @contact.notes.all
-    @search = Note.search(params[:search])
-    @notes = @search.select { |note| note[:contact_id] == @contact.id } unless @search.empty?
     @tasks = @contact.tasks.all
     @projects = @contact.projects.all
   end
