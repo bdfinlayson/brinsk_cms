7a8b718bbe654b7b3f8713ea0d3131e31980526f
Bryan Finlayson <bdfinlayson@gmail.com>
2016-11-12 19:07:44 -0600

    remove modal js

--- app/assets/javascripts/modal.js
+++
@@ -1,9 +0,0 @@
-$ ->
-  $("#modal-1").on "change", ->
-    if ($(this).is(":checked"))
-      $("body").addClass("modal-open")
-    else
-      $("body").removeClass("modal-open")
-
-  $(".modal-close").on "click", ->
-    $(".modal-state:checked").prop("checked", false).change()
