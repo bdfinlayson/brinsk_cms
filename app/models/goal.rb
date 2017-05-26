04d1785844636c97b0e6286d26620db8db8cc608
Bryan Finlayson <bryan.finlayson@metova.com>
2017-05-25 19:13:41 -0500

    add goals to homepage

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,5 +2,6 @@ class Goal < ApplicationRecord
   belongs_to :contact
   validates_presence_of :objective, :contact_id
 
-  scope :current_goals, -> { where(current: true) }
+  scope :current, -> { where(current: true) }
+  scope :achieved, -> { where(achieved: true) }
 end
