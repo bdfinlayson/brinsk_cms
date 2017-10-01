b025bae55230f422e7e3c48d6800216952a0b922
Bryan Finlayson <bryan.finlayson@metova.com>
2017-09-30 20:42:14 -0500

    expose header keys

--- config/initializers/rack_cors.rb
+++ config/initializers/rack_cors.rb
@@ -3,6 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'localhost:4200'
     resource '*',
       headers: :any,
+      expose:  %w(access-token expiry token-type uid client),
       methods: %i(get post put patch delete options head)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        sessions: 'api/v1/sessions'
+      }
     end
   end
 
